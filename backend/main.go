7e51e517bd6fd9df2d5cebd56ddce23f32895df3
László Ivicz <ivicz.laszlo.dr@gmail.com>
2025-04-23 16:38:56 +0200

    ALLOWED_ORIGIN environment variable

--- backend/main.go
+++ backend/main.go
@@ -68,7 +68,7 @@ func headers(c *gin.Context) {
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", os.Getenv("ALLOWED_ORIGIN"))
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PATCH, DELETE, PUT")
