27fbfd265b52bdb12ad3498dbad669d5d98f7d18
László Ivicz <ivicz.laszlo.dr@gmail.com>
2025-05-09 14:04:50 +0200

    tab selection with enter key

--- frontend/src/components/TabSelect/Tab.tsx
+++ frontend/src/components/TabSelect/Tab.tsx
@@ -11,11 +11,25 @@ export default function Tab({
   title: string
   onClick: (id: string) => void
 }) {
+  const keyUpHandler = (e: React.KeyboardEvent<HTMLDivElement>): void => {
+    if (e.key === 'Enter') {
+      onClick(id)
+    }
+  }
+
   if (id === selectedId) {
-    return <SelectedTab tabIndex={0}>{title}</SelectedTab>
+    return (
+      <SelectedTab tabIndex={0} onKeyUp={keyUpHandler}>
+        {title}
+      </SelectedTab>
+    )
   }
   return (
-    <UnselectedTab tabIndex={0} onClick={() => onClick(id)}>
+    <UnselectedTab
+      tabIndex={0}
+      onClick={() => onClick(id)}
+      onKeyUp={keyUpHandler}
+    >
       {title}
     </UnselectedTab>
   )
